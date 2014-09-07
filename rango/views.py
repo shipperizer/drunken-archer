8b9210742de5214e8f88a77030c4344a0e45b0a0
Alessandro Cabbia <alexcabb@gmail.com>
2014-09-07 16:31:36 +0100

    Chapter 7.2.4

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 
 from rango.models import Category, Page 
+from rango.forms import CategoryForm
+
 
 import utils
 
@@ -29,6 +31,18 @@ def category(request, category_name_url):
         pass
     return render_to_response('rango/category.html', context_dict, context)
 
-
 def about(request):
     return render(request, "rango/about.html")
+
+def add_category(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+    return render_to_response('rango/add_category.html', {'form': form}, context)
